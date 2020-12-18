f05a53774014a07a996558add7d9376570e52fe1
Erin Wrobel <ewrobel3@gatech.edu>
2020-12-18 17:36:18 -0500

    Initiating the design and connecting fontawesome

---
+++ script.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBREPOrDCDgf-ISwL2jZWaH4n3rx9Jr_Do",
+    authDomain: "work-hours-aa045.firebaseapp.com",
+    databaseURL: "https://work-hours-aa045-default-rtdb.firebaseio.com",
+    projectId: "work-hours-aa045",
+    storageBucket: "work-hours-aa045.appspot.com",
+    messagingSenderId: "951042764948",
+    appId: "1:951042764948:web:637b835b60fdf9858d4481"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
